675b07abe39abce3559f7f740ecbd5abcc81a89b
Nikola Kasev <nikola.kasev@gmail.com>
2020-07-08 22:39:20 +0200

    implemented input and output

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![Rust](https://github.com/nikolakasev/aoc2019-rust/workflows/Rust/badge.svg)
+
 # aoc2019-rust
 Learning Rust by applying it
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ fn str_to_intcode(string: &str) -> Vec<i32> {
 struct State {
     instruction_pointer: u32,
     intcode: Vec<i32>,
+    input: Vec<i32>,
+    output: Vec<i32>,
 }
 
 enum ComputeResult {
@@ -26,6 +28,8 @@ fn state_from_string(string: &str) -> State {
     State {
         instruction_pointer: 0,
         intcode: str_to_intcode(string),
+        input: vec![],
+        output: vec![],
     }
 }
 
@@ -47,8 +51,8 @@ fn computer(state: &mut State) -> Result<ComputeResult, String> {
 
         state.intcode[pos_to as usize] =
             state.intcode[pos_a as usize] + state.intcode[pos_b as usize];
-
         state.instruction_pointer += 4;
+
         computer(state)
     } else if opcode == 2 {
         let pos_a = state.intcode[(offset + 1) as usize];
@@ -57,14 +61,36 @@ fn computer(state: &mut State) -> Result<ComputeResult, String> {
 
         state.intcode[pos_to as usize] =
             state.intcode[pos_a as usize] * state.intcode[pos_b as usize];
-
         state.instruction_pointer += 4;
+
+        computer(state)
+    } else if opcode == 3 {
+        let pos_to = state.intcode[(offset + 1) as usize];
+
+        match state.input.pop() {
+            Some(v) => {
+                state.intcode[pos_to as usize] = v;
+                state.instruction_pointer += 2;
+
+                computer(state)
+            }
+            None => {
+                let error = format!("Input expected, but none provided.");
+                Err(error)
+            }
+        }
+    } else if opcode == 4 {
+        let pos_from = state.intcode[(offset + 1) as usize];
+
+        state.output.push(state.intcode[pos_from as usize]);
+        state.instruction_pointer += 2;
+
         computer(state)
     } else if opcode == 99 {
         Ok(Halt)
     } else {
         let error = format!("{} {}", "Unknown opcode", opcode);
-        return Err(error);
+        Err(error)
     }
 }
 
@@ -85,14 +111,23 @@ mod tests {
         let thirty = "1,1,1,4,99,5,6,0,99";
         let mut state = state_from_string(thirty);
 
-        assert_eq!(computer(&mut state).is_ok(), true);
+        assert_eq!(true, computer(&mut state).is_ok());
         assert_eq!(&30i32, state.intcode.first().unwrap())
     }
 
     #[test]
     fn day2_part_one() {
         let mut state = state_from_string("1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,9,23,27,1,5,27,31,1,5,31,35,1,35,13,39,1,39,9,43,1,5,43,47,1,47,6,51,1,51,13,55,1,55,9,59,1,59,13,63,2,63,13,67,1,67,10,71,1,71,6,75,2,10,75,79,2,10,79,83,1,5,83,87,2,6,87,91,1,91,6,95,1,95,13,99,2,99,13,103,1,103,9,107,1,10,107,111,2,111,13,115,1,10,115,119,1,10,119,123,2,13,123,127,2,6,127,131,1,13,131,135,1,135,2,139,1,139,6,0,99,2,0,14,0");
-        assert_eq!(computer(&mut state).is_ok(), true);
+        assert_eq!(true, computer(&mut state).is_ok());
         assert_eq!(&4090689i32, state.intcode.first().unwrap())
+    }
+
+    #[test]
+    fn input_output() {
+        let mut state = state_from_string("3,0,4,0,99");
+        state.input.push(55);
+
+        assert_eq!(true, computer(&mut state).is_ok());
+        assert_eq!(&55i32, state.output.first().unwrap())
     }
 }
