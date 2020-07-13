7e53c7252b793330ac444b57ee490af59dd07031
Nikola Kasev <nikola.kasev@gmail.com>
2020-07-13 23:34:54 +0200

    support for relative base, D9P1 solved

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ struct State {
     intcode: Vec<i64>,
     input: Vec<i64>,
     output: Vec<i64>,
+    relative_base: i64,
 }
 
 enum ComputeResult {
@@ -41,6 +42,7 @@ fn state_from_string(string: &str) -> State {
         intcode: str_to_intcode(string),
         input: vec![],
         output: vec![],
+        relative_base: 0,
     }
 }
 
@@ -49,19 +51,39 @@ fn get_value(parameter_mode: u8, pointer: u32, state: &State) -> i64 {
     //position mode
     if parameter_mode == 0 {
         let at_index = state.intcode[pointer as usize];
-        println!(
-            "pointer {}, got value {} at index {}",
-            pointer, state.intcode[at_index as usize], at_index
-        );
         state.intcode[at_index as usize]
     }
     //immediate mode
-    else {
-        println!(
-            "pointer {}, got value {} immediately",
-            pointer, state.intcode[pointer as usize]
-        );
+    else if parameter_mode == 1 {
         state.intcode[pointer as usize]
+    } else if parameter_mode == 2 {
+        let at_index = state.intcode[pointer as usize] + state.relative_base as i64;
+        state.intcode[at_index as usize]
+    } else {
+        panic!("parameter mode {} not supported", parameter_mode)
+    }
+}
+
+fn extend_memory(memory_index: u32, state: &mut State) {
+    if memory_index >= state.intcode.len() as u32 {
+        state.intcode.resize((memory_index + 1) as usize, 0);
+    }
+}
+
+fn get_memory_address(parameter_mode: u8, pointer: u32, state: &State) -> i64 {
+    //position mode
+    if parameter_mode == 0 {
+        state.intcode[pointer as usize]
+    }
+    //immediate mode
+    else if parameter_mode == 1 {
+        panic!("writing to memory will never be in immediate mode")
+    }
+    //relative mode
+    else if parameter_mode == 2 {
+        state.intcode[pointer as usize] + state.relative_base as i64
+    } else {
+        panic!("parameter mode {} not supported", parameter_mode)
     }
 }
 
@@ -77,38 +99,44 @@ fn computer(state: &mut State) -> Result<ComputeResult, String> {
     );
     assert!(state.intcode.len() > 0, "no intcode to process");
 
-    let (_, b, c, opcode) = parameter_modes(state.intcode[offset as usize]);
+    let (a, b, c, opcode) = parameter_modes(state.intcode[offset as usize]);
 
     //add
     if opcode == 1 {
-        let pos_to = state.intcode[(offset + 3) as usize];
+        let memory_address = get_memory_address(a, offset + 3, state);
+        extend_memory(memory_address as u32, state);
+
         let first_parameter = get_value(c, offset + 1, state);
         let second_parameter = get_value(b, offset + 2, state);
 
-        state.intcode[pos_to as usize] = first_parameter + second_parameter;
+        state.intcode[memory_address as usize] = first_parameter + second_parameter;
         state.instruction_pointer += 4;
 
         computer(state)
     }
     //multiply
     else if opcode == 2 {
-        let pos_to = state.intcode[(offset + 3) as usize];
+        let memory_address = get_memory_address(a, offset + 3, state);
+        extend_memory(memory_address as u32, state);
+
         let first_parameter = get_value(c, offset + 1, state);
         let second_parameter = get_value(b, offset + 2, state);
 
-        state.intcode[pos_to as usize] = first_parameter * second_parameter;
+        state.intcode[memory_address as usize] = first_parameter * second_parameter;
         state.instruction_pointer += 4;
 
         computer(state)
     }
     //input
     else if opcode == 3 {
-        let pos_to = state.intcode[(offset + 1) as usize];
+        let memory_address = get_memory_address(c, offset + 1, state);
 
         //attempt to read from the input
         match state.input.pop() {
             Some(v) => {
-                state.intcode[pos_to as usize] = v as i64;
+                extend_memory(memory_address as u32, state);
+
+                state.intcode[memory_address as usize] = v as i64;
                 state.instruction_pointer += 2;
 
                 computer(state)
@@ -155,8 +183,11 @@ fn computer(state: &mut State) -> Result<ComputeResult, String> {
         computer(state)
     }
     //less than
+    //todo refactor because the only difference in the logic for opcode 7 and 8 is '<' vs. '==', lambda or something?
     else if opcode == 7 {
-        let pos_to = state.intcode[(offset + 3) as usize];
+        let memory_address = get_memory_address(a, offset + 3, state);
+        extend_memory(memory_address as u32, state);
+
         let first_parameter = get_value(c, offset + 1, state);
         let second_parameter = get_value(b, offset + 2, state);
         let value = if first_parameter < second_parameter {
@@ -165,14 +196,16 @@ fn computer(state: &mut State) -> Result<ComputeResult, String> {
             0
         };
 
-        state.intcode[pos_to as usize] = value;
+        state.intcode[memory_address as usize] = value;
         state.instruction_pointer += 4;
 
         computer(state)
     }
     //equals
     else if opcode == 8 {
-        let pos_to = state.intcode[(offset + 3) as usize];
+        let memory_address = get_memory_address(a, offset + 3, state);
+        extend_memory(memory_address as u32, state);
+
         let first_parameter = get_value(c, offset + 1, state);
         let second_parameter = get_value(b, offset + 2, state);
         let value = if first_parameter == second_parameter {
@@ -181,8 +214,17 @@ fn computer(state: &mut State) -> Result<ComputeResult, String> {
             0
         };
 
-        state.intcode[pos_to as usize] = value;
+        state.intcode[memory_address as usize] = value;
         state.instruction_pointer += 4;
+
+        computer(state)
+    }
+    //adjust relative base
+    else if opcode == 9 {
+        let first_parameter = get_value(c, offset + 1, state);
+
+        state.relative_base += first_parameter;
+        state.instruction_pointer += 2;
 
         computer(state)
     } else if opcode == 99 {
@@ -260,12 +302,24 @@ mod tests {
         assert_eq!(state.output, vec![2140710])
     }
 
-    fn input_day7() -> &'static str {
-        "3,8,1001,8,10,8,105,1,0,0,21,42,67,84,97,118,199,280,361,442,99999,3,9,101,4,9,9,102,5,9,9,101,2,9,9,1002,9,2,9,4,9,99,3,9,101,5,9,9,102,5,9,9,1001,9,5,9,102,3,9,9,1001,9,2,9,4,9,99,3,9,1001,9,5,9,1002,9,2,9,1001,9,5,9,4,9,99,3,9,1001,9,5,9,1002,9,3,9,4,9,99,3,9,102,4,9,9,101,4,9,9,102,2,9,9,101,3,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,99"
-    }
+    //    fn input_day7() -> &'static str {
+    //        "3,8,1001,8,10,8,105,1,0,0,21,42,67,84,97,118,199,280,361,442,99999,3,9,101,4,9,9,102,5,9,9,101,2,9,9,1002,9,2,9,4,9,99,3,9,101,5,9,9,102,5,9,9,1001,9,5,9,102,3,9,9,1001,9,2,9,4,9,99,3,9,1001,9,5,9,1002,9,2,9,1001,9,5,9,4,9,99,3,9,1001,9,5,9,1002,9,3,9,4,9,99,3,9,102,4,9,9,101,4,9,9,102,2,9,9,101,3,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,99"
+    //    }
 
     fn input_day9() -> &'static str {
         "1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,3,1,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,30,1010,1102,1,38,1008,1102,1,0,1020,1102,22,1,1007,1102,26,1,1015,1102,31,1,1013,1102,1,27,1014,1101,0,23,1012,1101,0,37,1006,1102,735,1,1028,1102,1,24,1009,1102,1,28,1019,1102,20,1,1017,1101,34,0,1001,1101,259,0,1026,1101,0,33,1018,1102,1,901,1024,1101,21,0,1016,1101,36,0,1011,1102,730,1,1029,1101,1,0,1021,1102,1,509,1022,1102,39,1,1005,1101,35,0,1000,1102,1,506,1023,1101,0,892,1025,1101,256,0,1027,1101,25,0,1002,1102,1,29,1004,1102,32,1,1003,109,9,1202,-3,1,63,1008,63,39,63,1005,63,205,1001,64,1,64,1106,0,207,4,187,1002,64,2,64,109,-2,1208,-4,35,63,1005,63,227,1001,64,1,64,1105,1,229,4,213,1002,64,2,64,109,5,1206,8,243,4,235,1106,0,247,1001,64,1,64,1002,64,2,64,109,14,2106,0,1,1105,1,265,4,253,1001,64,1,64,1002,64,2,64,109,-25,1201,4,0,63,1008,63,40,63,1005,63,285,1106,0,291,4,271,1001,64,1,64,1002,64,2,64,109,14,2107,37,-7,63,1005,63,313,4,297,1001,64,1,64,1106,0,313,1002,64,2,64,109,-7,21101,40,0,5,1008,1013,37,63,1005,63,333,1105,1,339,4,319,1001,64,1,64,1002,64,2,64,109,-7,1207,0,33,63,1005,63,355,1106,0,361,4,345,1001,64,1,64,1002,64,2,64,109,7,21102,41,1,9,1008,1017,41,63,1005,63,387,4,367,1001,64,1,64,1106,0,387,1002,64,2,64,109,-1,21102,42,1,10,1008,1017,43,63,1005,63,411,1001,64,1,64,1106,0,413,4,393,1002,64,2,64,109,-5,21101,43,0,8,1008,1010,43,63,1005,63,435,4,419,1106,0,439,1001,64,1,64,1002,64,2,64,109,16,1206,3,455,1001,64,1,64,1106,0,457,4,445,1002,64,2,64,109,-8,21107,44,45,7,1005,1017,479,4,463,1001,64,1,64,1106,0,479,1002,64,2,64,109,6,1205,5,497,4,485,1001,64,1,64,1106,0,497,1002,64,2,64,109,1,2105,1,6,1105,1,515,4,503,1001,64,1,64,1002,64,2,64,109,-10,2108,36,-1,63,1005,63,535,1001,64,1,64,1105,1,537,4,521,1002,64,2,64,109,-12,2101,0,6,63,1008,63,32,63,1005,63,561,1001,64,1,64,1105,1,563,4,543,1002,64,2,64,109,25,21108,45,46,-2,1005,1018,583,1001,64,1,64,1105,1,585,4,569,1002,64,2,64,109,-23,2108,34,4,63,1005,63,607,4,591,1001,64,1,64,1106,0,607,1002,64,2,64,109,3,1202,7,1,63,1008,63,22,63,1005,63,633,4,613,1001,64,1,64,1106,0,633,1002,64,2,64,109,12,21108,46,46,3,1005,1015,651,4,639,1106,0,655,1001,64,1,64,1002,64,2,64,109,-5,2102,1,-1,63,1008,63,35,63,1005,63,679,1001,64,1,64,1105,1,681,4,661,1002,64,2,64,109,13,21107,47,46,-7,1005,1013,701,1001,64,1,64,1105,1,703,4,687,1002,64,2,64,109,-2,1205,2,715,1106,0,721,4,709,1001,64,1,64,1002,64,2,64,109,17,2106,0,-7,4,727,1105,1,739,1001,64,1,64,1002,64,2,64,109,-23,2107,38,-6,63,1005,63,759,1001,64,1,64,1106,0,761,4,745,1002,64,2,64,109,-3,1207,-4,40,63,1005,63,779,4,767,1105,1,783,1001,64,1,64,1002,64,2,64,109,-8,2101,0,-1,63,1008,63,35,63,1005,63,809,4,789,1001,64,1,64,1105,1,809,1002,64,2,64,109,-6,2102,1,8,63,1008,63,32,63,1005,63,835,4,815,1001,64,1,64,1106,0,835,1002,64,2,64,109,6,1201,5,0,63,1008,63,37,63,1005,63,857,4,841,1106,0,861,1001,64,1,64,1002,64,2,64,109,2,1208,0,32,63,1005,63,883,4,867,1001,64,1,64,1106,0,883,1002,64,2,64,109,23,2105,1,-2,4,889,1001,64,1,64,1106,0,901,4,64,99,21102,27,1,1,21101,0,915,0,1106,0,922,21201,1,55337,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21101,0,942,0,1105,1,922,21202,1,1,-1,21201,-2,-3,1,21102,957,1,0,1105,1,922,22201,1,-1,-2,1106,0,968,21201,-2,0,-2,109,-3,2105,1,0"
+    }
+
+    #[test]
+    fn relative_base() {
+        let mut state =
+            state_from_string("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99");
+
+        assert_eq!(computer(&mut state).is_ok(), true);
+        assert_eq!(
+            state.output,
+            vec![109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+        )
     }
 
     #[test]
@@ -273,6 +327,29 @@ mod tests {
         let mut state = state_from_string("104,1125899906842624,99");
 
         assert_eq!(computer(&mut state).is_ok(), true);
-        assert_eq!(state.output.first().unwrap(), &1125899906842624i64)
+        assert_eq!(state.output, vec![1125899906842624]);
+
+        state = state_from_string("1102,34915192,34915192,7,4,7,99,0");
+
+        assert_eq!(computer(&mut state).is_ok(), true);
+        assert_eq!(state.output, vec![1219070632396864]);
     }
+
+    #[test]
+    fn day9_part_one() {
+        let mut state = state_from_string(input_day9());
+        state.input.push(1);
+
+        assert_eq!(computer(&mut state).is_ok(), true);
+        assert_eq!(state.output, vec![3765554916])
+    }
+
+    //    #[test]
+    //    fn day9_part_two() {
+    //        let mut state = state_from_string(input_day9());
+    //        state.input.push(2);
+    //
+    //        assert_eq!(computer(&mut state).is_ok(), true);
+    //        assert_eq!(state.output, vec![2140710])
+    //    }
 }
