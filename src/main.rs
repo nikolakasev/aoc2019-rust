8530edc8ebd064d00af3590759a602c083c2512a
Nikola Kasev <nikola.kasev@gmail.com>
2020-11-10 16:30:08 +0100

    addressed warnings from clippy

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ fn main() {
     let input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,9,23,27,1,5,27,31,1,5,31,35,1,35,13,39,1,39,9,43,1,5,43,47,1,47,6,51,1,51,13,55,1,55,9,59,1,59,13,63,2,63,13,67,1,67,10,71,1,71,6,75,2,10,75,79,2,10,79,83,1,5,83,87,2,6,87,91,1,91,6,95,1,95,13,99,2,99,13,103,1,103,9,107,1,10,107,111,2,111,13,115,1,10,115,119,1,10,119,123,2,13,123,127,2,6,127,131,1,13,131,135,1,135,2,139,1,139,6,0,99,2,0,14,0";
 
     println!("Hello, world, {:?}", str_to_intcode(input));
-    //    println!("{:?}", parameter_modes(1009));
 }
 
 fn str_to_intcode(string: &str) -> Vec<i64> {
     string
-        .split_terminator(",")
+        .split_terminator(',')
         .map(|s| s.parse().unwrap())
         .collect()
 }
@@ -106,7 +105,7 @@ fn five_amplifiers_in_sequence(intcode: &str, phase_setting: Vec<i64>) -> i64 {
         .unwrap()
 }
 
-fn computer(intcode: &str, input: Vec<i64>) -> Result<Vec<i64>, &str> {
+fn computer(intcode: &str, input: Vec<i64>) -> Result<Vec<i64>, String> {
     let mut state = state_from_string(intcode);
     state.input = input;
 
@@ -117,7 +116,7 @@ fn computer(intcode: &str, input: Vec<i64>) -> Result<Vec<i64>, &str> {
                 CanContinue => continue,
             },
             //todo refactor the nested match and simplify the error mapping
-            Err(_) => break Err("bam"),
+            Err(e) => break Err(e),
         }
     }
 }
@@ -177,8 +176,7 @@ fn five_amplifiers_in_a_feedback_loop(
 
                     CanContinue => continue,
                 },
-                //todo refactor the nested match and simplify the error mapping
-                Err(_) => panic!("{} ",),
+                Err(e) => panic!("{} error: {}", name, e),
             }
         }
     };
@@ -209,7 +207,7 @@ fn compute(state: &mut State) -> Result<ComputeResult, String> {
         offset,
         state.intcode.len()
     );
-    assert!(state.intcode.len() > 0, "no intcode to process");
+    assert!(!state.intcode.is_empty(), "no intcode to process");
 
     let (a, b, c, opcode) = parameter_modes(state.intcode[offset as usize]);
 
